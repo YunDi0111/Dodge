04ebe19e5267db63168098864ecaa6e8cc31360f
YunDi <qjqmf4646@naver.com>
2022-09-15 17:32:49 +0900

    20220915

--- Dodge/Assets/PlayerController.cs
+++ Dodge/Assets/PlayerController.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRididbody;   //이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody playerRigidbody;   //이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f;    //이동속력
     
     void Start()
     {   //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
-        playerRididbody = GetComponent<Rigidbody>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {   //위 방향키 입력이 감지된 경우 z방향 힘주기
-            playerRididbody.AddForce(0f, 0f, speed);
+            playerRigidbody.AddForce(0f, 0f, speed);
         }
 
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {   //아래 방향키 입력이 감지된 경우 -z방향 힘주기
-            playerRididbody.AddForce(0f, 0f, -speed);
+            playerRigidbody.AddForce(0f, 0f, -speed);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {   //오른 방향키 입력이 감지된 경우 x방향 힘주기
-            playerRididbody.AddForce(speed, 0f, 0f);
+            playerRigidbody.AddForce(speed, 0f, 0f);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {   //왼 방향키 입력이 감지된 경우 -x방향 힘주기
-            playerRididbody.AddForce(-speed, 0f, 0f);
+            playerRigidbody.AddForce(-speed, 0f, 0f);
         }
     }
     public void Die()
